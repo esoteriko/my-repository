e8348aad75ab65c05022332bd6e1f4f45d4746fa
Saul Lopez <slopez@grupoabraxas.com>
2018-07-17 14:02:13 -0500

    MY second script

---
+++ social_service.rb
@@ -0,0 +1,9 @@
+require 'elasticsearch'
+
+def client
+	client = Elasticsearch::Client.new trace: true
+end
+
+@date = gets
+@hours = gets
+client.index index: 'horas_v1', type: 'datos', body:{date: @date, hours: @hours}# my-repository
